2ef75be87894807595c22312597e8911fd929a44
Natanael Borges <natanael.dr.borges@gmail.com>
2022-12-28 22:41:30 +1100

    docs: create open API

--- src/infrastructure/helpers/config/api.server.config.ts
+++ src/infrastructure/helpers/config/api.server.config.ts
@@ -3,6 +3,12 @@ export class ApiServerConfig {
 
   public static readonly PORT: number = parseInt(process.env.PORT);
 
+  public static readonly APP_NAME: string = process.env.APP_NAME;
+
+  public static readonly APP_DESCRIPTION: string = process.env.APP_DESCRIPTION;
+
+  public static readonly API_VERSION: string = process.env.API_VERSION;
+
   public static readonly API_ACCESS_TOKEN_HEADER: string =
     process.env.API_ACCESS_TOKEN_HEADER;
 }
--- src/infrastructure/setup/server.application.ts
+++ src/infrastructure/setup/server.application.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { Logger } from '@nestjs/common';
+import { DocumentBuilder, OpenAPIObject, SwaggerModule } from '@nestjs/swagger';
 import { RootModule } from '@infrastructure/modules/root.module';
 import { ApiServerConfig } from '@infrastructure/helpers/config/api.server.config';
 
@@ -15,6 +16,8 @@ export class ServerApplication {
         await NestFactory.create<NestExpressApplication>(RootModule);
 
       this.log();
+      this.buildAPIDocumentation(app);
+
       await app.listen(this.port, this.host);
     } catch (error) {
       Logger.error(`❌  Error starting server, ${error}`, 'Bootstrap');
@@ -32,6 +35,33 @@ export class ServerApplication {
       `🚀  Server ready at http://${this.host}:${this.port}`,
       'Bootstrap',
     );
+  }
+
+  private buildAPIDocumentation(app: NestExpressApplication): void {
+    const title = ApiServerConfig.APP_NAME;
+    const description = ApiServerConfig.APP_DESCRIPTION;
+    const version = ApiServerConfig.API_VERSION;
+
+    const options: Omit<OpenAPIObject, 'paths'> = new DocumentBuilder()
+      .setTitle(title)
+      .setDescription(description)
+      .setVersion(version)
+      .addBearerAuth({
+        type: 'apiKey',
+        in: 'header',
+        name: ApiServerConfig.API_ACCESS_TOKEN_HEADER,
+      })
+      .build();
+
+    const document: OpenAPIObject = SwaggerModule.createDocument(app, options);
+    SwaggerModule.setup('documentation', app, document);
+    SwaggerModule.setup('docs', app, document);
+
+    Logger.log(
+      'Mapped {/documentation, GET} Swagger api route',
+      'RouterExplorer',
+    );
+    Logger.log('Mapped {/docs, GET} Swagger api route', 'RouterExplorer');
   }
 
   public static new(): ServerApplication {
